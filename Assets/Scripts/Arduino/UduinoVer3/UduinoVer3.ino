4b652db698b887ee3d4932f07bfaf22da8019ba3
Anylights <Anylights@outlook.com>
2025-04-28 00:05:21 +0800

    这一版有最基础的灯光

--- Assets/Scripts/Arduino/UduinoVer3/UduinoVer3.ino
+++ Assets/Scripts/Arduino/UduinoVer3/UduinoVer3.ino
@@ -10,17 +10,15 @@
 Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(LED_COUNT, LED_PIN_1, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(LED_COUNT, LED_PIN_2, NEO_GRB + NEO_KHZ800);
 
-
 Uduino uduino("uduinoBoard"); // Declare and name your object
 
 // Servo
 #include <Servo.h>
 #define MAXSERVOS 8
 
-
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   strip1.begin();
   strip2.begin();
